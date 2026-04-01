3e02fe0973674f28a2bcba7b6c80dda810de602f
mohitshaw1496 <darkdictator691@gmail.com>
2026-04-02 01:39:27 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -372,7 +372,7 @@ async def drm_handler(bot: Client, m: Message):
                     else:
                         if caption == "/cc1":
                             cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{name1} [{res}p].mkv`\n<blockquote><b>`**➭ 𝐁𝐚𝐭𝐜𝐡 »</b> {b_name}**</blockquote>\n\n**Extracted by➤**{CR}\n'
-                            cc1 = f'➭ **Index »** {str(count).zfill(3)}\n**➭ Title »** `{name1}.pdf`\n<blockquote><b>**➭ 𝐁𝐚𝐭𝐜𝐡 » {b_name}**\n</blockquote>\n✨ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐃 𝐁𝐘 **:**{CR}\n━━━━━━━✦✗✦━━━━━━━\n'
+                            cc1 = f'➭ **Index » {str(count).zfill(3)}**\n**➭ Title » `{name1}.pdf`**\n<blockquote><b>**➭ 𝐁𝐚𝐭𝐜𝐡 » {b_name}**\n</blockquote>✨ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐃 𝐁𝐘 **:**{CR}\n━━━━━━━✦✗✦━━━━━━━\n'
                             cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1}.zip`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n' 
                             ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1}.jpg`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
                             ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1}.mp3`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
