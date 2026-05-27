e8d0cd64dbc552b764dc77fd5b44b6cdb7ee5098
mohitshaw1496 <darkdictator691@gmail.com>
2026-05-27 16:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return """
+@app.route("/")
+def home():
+    return "Flask app is running"
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,5 +45,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
